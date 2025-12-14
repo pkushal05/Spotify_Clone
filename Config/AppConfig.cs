d5ecc2c4904592f541850227ce48003fcdefc218
pkushal05 <pkushal1052@gmail.com>
2025-12-14 15:14:57 -0500

    App Config instructions

--- Config/AppConfig.cs
+++ Config/AppConfig.cs
@@ -9,11 +9,11 @@ namespace Spotify_Clone.Config
     public class AppConfig
     {
         // Spotify API Credentials
-        public static string ClientId = "";
-        public static string ClientSecret = "";
+        public static string ClientId = "YOUR_CLIENT_ID_HERE";
+        public static string ClientSecret = "YOUR_CLIENT_SECRET_HERE";
 
         // Spotify API Access Token
-        public static string AccessToken = "";
+        public static string AccessToken = "BQA6tP1m...paste_your_token_here...xyz";
 
         // Spotify API Base URL
         public static string BaseUrl = "https://api.spotify.com/v1/";
